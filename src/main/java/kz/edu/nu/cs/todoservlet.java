83a948ce938dfac5eac97a300af7142f6a687aca
Alpamys Duimagambetov <alpamys.duimagambetov@nu.edu.kz>
2018-09-15 14:55:09 +0600

    ToDoLab

--- src/main/java/kz/edu/nu/cs/todoservlet.java
+++ src/main/java/kz/edu/nu/cs/todoservlet.java
@@ -17,16 +17,17 @@ import com.google.gson.Gson;
 public class todoservlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static List<String> list = new ArrayList<String>();
-       
 
-    public todoservlet() {
-        super();
-        list.add("Welcome, start adding items!");
-    }
+
+	public todoservlet() {
+		super();
+		list.add("Welcome, start adding items!!!");
+	}
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
+		System.out.println("doGet");
 		response.setContentType("text/plain");
 		Gson gson = new Gson();
 		out.append(gson.toJson(list));
@@ -34,8 +35,28 @@ public class todoservlet extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    String myText = request.getParameter("texttosend");
-	    list.add(myText);
+		String myText = request.getParameter("texttosend");
+		System.out.println("doPost");
+		list.add(myText);
+	}
+
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+
+		list.remove(id);
+		System.out.println(list);
+
+	}
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPut");
+
+		int id = Integer.parseInt(request.getParameter("textid"));
+
+		String to = request.getParameter("textto");
+		System.out.println(id + " " + to);
+		list.set(id,to);
+
 	}
 
 }
